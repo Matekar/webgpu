f09ff66cbc663f1f090098a6361b6082d11d045d
Marcin Kiżewski <kiewskimarcin@gmail.com>
2024-07-12 18:32:21 +0200

    Updated Model parameters

--- src/interfaces/Model.ts
+++ src/interfaces/Model.ts
@@ -1,10 +1,9 @@
 import { ReadonlyMat4, mat4, vec3 } from "gl-matrix";
-import { Mesh } from "./Mesh";
 
 export interface Model {
   position: vec3;
   eulers?: vec3;
-  scaler?: vec3;
+  scale?: vec3;
 
   model: mat4;
 
--- src/model/basicModel.ts
+++ src/model/basicModel.ts
@@ -1,24 +1,27 @@
 import { ReadonlyMat4, mat4, vec3 } from "gl-matrix";
 import { Model } from "../interfaces/Model";
-import { Mesh } from "../interfaces/Mesh";
 
 export class BasicModel implements Model {
   position: vec3;
   eulers: vec3;
-  scaler: vec3;
+  scale: vec3;
 
   model!: mat4;
 
-  constructor(position: vec3) {
+  constructor(
+    position: vec3,
+    eulers: vec3 = vec3.create(),
+    scale: vec3 = vec3.fromValues(1, 1, 1)
+  ) {
     this.position = position;
-    this.eulers = vec3.create();
-    this.scaler = vec3.fromValues(1, 1, 1);
+    this.eulers = eulers;
+    this.scale = scale;
   }
 
   update(): BasicModel {
     this.model = mat4.create();
     mat4.translate(this.model, this.model, this.position);
-    mat4.scale(this.model, this.model, this.scaler);
+    mat4.scale(this.model, this.model, this.scale);
 
     return this;
   }
