5a2b0c7d21e4026e6ff3c6c9ec237bc07a24996b
Marcin Kiżewski <kiewskimarcin@gmail.com>
2024-08-03 16:54:15 +0200

    Added cDebugInfo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Renderer } from "../view/renderer";
 import { Scene } from "../model/scene";
 import { RenderMode } from "../interfaces/enums";
-import { rad2deg, vecsToRotation } from "../utility/mathUtilities";
+import cDebugInfo from "./debugInfo";
 
 export class App {
   canvas: HTMLCanvasElement;
@@ -14,17 +14,6 @@ export class App {
   // FIXME: reorganize querying HTML Elements do display debug info
   // FIXME: move HTMLElement to separate object/function/singleton
 
-  keyLabel: HTMLElement;
-  mouseXLabel: HTMLElement;
-  mouseYLabel: HTMLElement;
-  cameraPositionLabel: HTMLElement;
-
-  // camera orientation labels
-  cameraOrientationLabel: HTMLElement;
-  cameraForwardsLabel: HTMLElement;
-  cameraRightLabel: HTMLElement;
-  cameraUpLabel: HTMLElement;
-
   moveMap: Map<String, Boolean>;
   acceleration: number;
 
@@ -32,18 +21,6 @@ export class App {
     this.canvas = canvas;
     this.renderer = new Renderer();
     this.scene = new Scene();
-
-    this.keyLabel = document.querySelector("#key_label")!;
-    this.mouseXLabel = document.querySelector("#mouse_x_label")!;
-    this.mouseYLabel = document.querySelector("#mouse_y_label")!;
-    // prettier-ignore
-    this.cameraPositionLabel = document.querySelector("#camera_position_label")!;
-    // prettier-ignore
-    this.cameraOrientationLabel = document.querySelector("#camera_orientation_label")!;
-    // prettier-ignore
-    this.cameraForwardsLabel = document.querySelector("#camera_forwards_label")!;
-    this.cameraRightLabel = document.querySelector("#camera_right_label")!;
-    this.cameraUpLabel = document.querySelector("#camera_up_label")!;
 
     document.addEventListener("keydown", (event) => this._handleKeydown(event));
     document.addEventListener("keyup", (event) => this._handleKeyup(event));
@@ -97,7 +74,7 @@ export class App {
 
     this.renderer.render(this.scene.getRenderables());
 
-    this._printDebugInfo();
+    cDebugInfo.printDebugInfo(this.scene, this.moveMap);
 
     if (running) {
       requestAnimationFrame(this.run);
@@ -134,66 +111,11 @@ export class App {
 
   _handleMousemove(event: MouseEvent) {
     // FIXME: move to _printDebugInfo
-    this.mouseXLabel.innerText =
-      "Mouse X acceleration: " + event.movementX.toString();
-    this.mouseYLabel.innerText =
-      "Mouse Y acceleration: " + event.movementY.toString();
+    cDebugInfo.registerMouseInfo(
+      [event.clientX, event.clientY],
+      [event.movementX, event.movementY]
+    );
 
     this.scene.spinPlayer(event.movementX * 0.2, event.movementY * 0.2);
   }
-
-  _printDebugInfo = () => {
-    this.keyLabel.innerText = "Current keys: [";
-    this.moveMap.forEach(
-      (v, k) => (this.keyLabel.innerText += v ? k + " " : "")
-    );
-    this.keyLabel.innerText += "]";
-
-    const [fAngle, rAngle, uAngle] = vecsToRotation(
-      this.scene.player.forward,
-      this.scene.player.right,
-      this.scene.player.up
-    );
-
-    this.cameraPositionLabel.innerHTML =
-      "Camera position: " +
-      Math.floor((this.scene.player.position[0] + Number.EPSILON) * 100) / 100 +
-      ", " +
-      Math.floor((this.scene.player.position[1] + Number.EPSILON) * 100) / 100 +
-      ", " +
-      Math.floor((this.scene.player.position[2] + Number.EPSILON) * 100) / 100;
-
-    this.cameraForwardsLabel.innerHTML =
-      " Forward: " +
-      Math.floor((this.scene.player.forward[0] + Number.EPSILON) * 100) / 100 +
-      ", " +
-      Math.floor((this.scene.player.forward[1] + Number.EPSILON) * 100) / 100 +
-      ", " +
-      Math.floor((this.scene.player.forward[2] + Number.EPSILON) * 100) / 100 +
-      " PSI:(" +
-      Math.floor((rad2deg(fAngle) + Number.EPSILON) * 100) / 100 +
-      ")";
-
-    this.cameraRightLabel.innerHTML =
-      " Right: " +
-      Math.floor((this.scene.player.right[0] + Number.EPSILON) * 100) / 100 +
-      ", " +
-      Math.floor((this.scene.player.right[1] + Number.EPSILON) * 100) / 100 +
-      ", " +
-      Math.floor((this.scene.player.right[2] + Number.EPSILON) * 100) / 100 +
-      " THETA:(" +
-      Math.floor((rad2deg(rAngle) + Number.EPSILON) * 100) / 100 +
-      ")";
-
-    this.cameraUpLabel.innerHTML =
-      " Up: " +
-      Math.floor((this.scene.player.up[0] + Number.EPSILON) * 100) / 100 +
-      ", " +
-      Math.floor((this.scene.player.up[1] + Number.EPSILON) * 100) / 100 +
-      ", " +
-      Math.floor((this.scene.player.up[2] + Number.EPSILON) * 100) / 100 +
-      " PHI:(" +
-      Math.floor((rad2deg(uAngle) + Number.EPSILON) * 100) / 100 +
-      ")";
-  };
 }
---
+++ src/app/debugInfo.ts
@@ -0,0 +1,95 @@
+import { Scene } from "../model/scene";
+import { rad2deg, vecsToRotation } from "../utility/mathUtilities";
+
+class DebugInfo {
+  keyLabel: HTMLElement;
+
+  // mouse info
+  mousePositionLabel: HTMLElement;
+  mouseAccelerationLabel: HTMLElement;
+
+  // camera info
+  cameraPositionLabel: HTMLElement;
+  cameraOrientationLabel: HTMLElement;
+  cameraForwardsLabel: HTMLElement;
+  cameraRightLabel: HTMLElement;
+  cameraUpLabel: HTMLElement;
+
+  constructor() {
+    this.keyLabel = this.queryOne("#key_label")!;
+    this.mousePositionLabel = this.queryOne("#mouse_position_label")!;
+    this.mouseAccelerationLabel = this.queryOne("#mouse_acceleration_label")!;
+    this.cameraPositionLabel = this.queryOne("#camera_position_label")!;
+    this.cameraOrientationLabel = this.queryOne("#camera_orientation_label")!;
+    this.cameraForwardsLabel = this.queryOne("#camera_forwards_label")!;
+    this.cameraRightLabel = this.queryOne("#camera_right_label")!;
+    this.cameraUpLabel = this.queryOne("#camera_up_label")!;
+  }
+
+  queryOne = (selector: string): HTMLElement =>
+    document.querySelector(selector)!;
+
+  registerMouseInfo = (position: number[], acceleration: number[]) => {
+    this.mousePositionLabel.innerHTML =
+      "Mouse position (XY): " + position.toString();
+    this.mouseAccelerationLabel.innerHTML =
+      "Mouse acceleration (XY): " + acceleration.toString();
+  };
+
+  printDebugInfo = (scene: Scene, moveMap: Map<String, Boolean>) => {
+    this.keyLabel.innerText = "Current keys: [";
+    moveMap.forEach((v, k) => (this.keyLabel.innerText += v ? k + " " : ""));
+    this.keyLabel.innerText += "]";
+
+    const [fAngle, rAngle, uAngle] = vecsToRotation(
+      scene.player.forward,
+      scene.player.right,
+      scene.player.up
+    );
+
+    this.cameraPositionLabel.innerHTML =
+      "Camera position: " +
+      Math.floor((scene.player.position[0] + Number.EPSILON) * 100) / 100 +
+      ", " +
+      Math.floor((scene.player.position[1] + Number.EPSILON) * 100) / 100 +
+      ", " +
+      Math.floor((scene.player.position[2] + Number.EPSILON) * 100) / 100;
+
+    this.cameraForwardsLabel.innerHTML =
+      " Forward: " +
+      Math.floor((scene.player.forward[0] + Number.EPSILON) * 100) / 100 +
+      ", " +
+      Math.floor((scene.player.forward[1] + Number.EPSILON) * 100) / 100 +
+      ", " +
+      Math.floor((scene.player.forward[2] + Number.EPSILON) * 100) / 100 +
+      " PSI:(" +
+      Math.floor((rad2deg(fAngle) + Number.EPSILON) * 100) / 100 +
+      ")";
+
+    this.cameraRightLabel.innerHTML =
+      " Right: " +
+      Math.floor((scene.player.right[0] + Number.EPSILON) * 100) / 100 +
+      ", " +
+      Math.floor((scene.player.right[1] + Number.EPSILON) * 100) / 100 +
+      ", " +
+      Math.floor((scene.player.right[2] + Number.EPSILON) * 100) / 100 +
+      " THETA:(" +
+      Math.floor((rad2deg(rAngle) + Number.EPSILON) * 100) / 100 +
+      ")";
+
+    this.cameraUpLabel.innerHTML =
+      " Up: " +
+      Math.floor((scene.player.up[0] + Number.EPSILON) * 100) / 100 +
+      ", " +
+      Math.floor((scene.player.up[1] + Number.EPSILON) * 100) / 100 +
+      ", " +
+      Math.floor((scene.player.up[2] + Number.EPSILON) * 100) / 100 +
+      " PHI:(" +
+      Math.floor((rad2deg(uAngle) + Number.EPSILON) * 100) / 100 +
+      ")";
+  };
+}
+
+const cDebugInfo = new DebugInfo();
+
+export default cDebugInfo;
