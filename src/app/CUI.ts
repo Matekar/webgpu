dbac23b6f42510704bc70812afaa90d654455c4b
Marcin Kiżewski <kiewskimarcin@gmail.com>
2024-09-22 15:00:57 +0200

    Added Tree crawling

--- src/app/CUI.ts
+++ src/app/CUI.ts
@@ -1,4 +1,6 @@
 class CommandNode {
+  private _id: number;
+
   private _name: string;
   private _description: string;
   private _invokeBy: string;
@@ -6,17 +8,24 @@ class CommandNode {
   private _children: Array<CommandNode>;
 
   constructor(
+    id: number,
     invokeBy: string,
     commandKey: string,
     name: string = "",
     description: string = ""
   ) {
+    this._id = id;
+
     this._name = name;
     this._description = description;
 
     this._invokeBy = invokeBy;
     this._commandKey = commandKey;
     this._children = new Array();
+  }
+
+  public get ID(): number {
+    return this._id;
   }
 
   public get name(): string {
@@ -31,8 +40,19 @@ class CommandNode {
     return this._invokeBy;
   }
 
+  public get commandKey(): string {
+    return this._commandKey;
+  }
+
   public get children(): Array<CommandNode> {
     return this._children;
+  }
+
+  public searchIn(id: number): CommandNode | undefined {
+    if (this._id == id) return this;
+    if (this._children.length == 0) return undefined;
+
+    return this._children.find((child) => child.searchIn(id));
   }
 }
 
@@ -42,8 +62,9 @@ class CUI {
   private _commands: Map<string, Function>;
   private _mainRoot: CommandNode;
   private _subRoots: Array<CommandNode>;
+  private _currentSubRoot: number;
 
-  private _eventStack: string[];
+  private _invokeStack!: Map<string, number>;
 
   constructor(mainInvoke: string, areEnabledOnStart: boolean = true) {
     this._areShortCommandsEnabled = areEnabledOnStart;
@@ -51,18 +72,41 @@ class CUI {
     this._commands = new Map();
     this._commands.set("", this.invokeCommand);
 
-    this._mainRoot = new CommandNode(mainInvoke, "");
+    this._mainRoot = new CommandNode(0, mainInvoke, "");
     this._subRoots = new Array();
+    this._currentSubRoot = 0;
 
-    this._eventStack = [];
-    this.populateStack();
+    this._populateStack();
+
+    document.addEventListener("keydown", (event) => this._handleKeydown(event));
+    document.addEventListener("keyup", (event) => this._handleKeyup(event));
   }
 
-  private populateStack(branch: Array<CommandNode> = this._subRoots) {
-    this._eventStack = [this._mainRoot.invoker];
+  private _handleKeydown(ev: KeyboardEvent) {
+    keyMap.set(ev.code, true);
+
+    if (this._invokeStack.get(ev.key)) {
+      this._populateStack(
+        this._subRoots[this._currentSubRoot].searchIn(
+          this._invokeStack.get(ev.key)!
+        )!.children
+      );
+      return;
+    }
+  }
+
+  private _handleKeyup(ev: KeyboardEvent) {
+    keyMap.set(ev.code, false);
+  }
+
+  private _populateStack(
+    branch: Array<CommandNode> = [this._subRoots[this._currentSubRoot]]
+  ) {
+    this._invokeStack = new Map();
+    this._invokeStack.set(this._mainRoot.invoker, this._mainRoot.ID);
 
     if (this._areShortCommandsEnabled) {
-      branch.forEach((node) => this._eventStack.push(node.invoker));
+      branch.forEach((node) => this._invokeStack.set(node.invoker, node.ID));
     }
   }
 
@@ -76,7 +120,7 @@ class CUI {
   }
 }
 
+const keyMap = new Map<string, boolean>();
+
 export const cCUI = new CUI(":");
 export default cCUI;
-
-export const keyMap = new Map<string, boolean>();
